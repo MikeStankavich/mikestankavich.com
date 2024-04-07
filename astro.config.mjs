4c77fba20d3d10cf00bdec87bc62a2070de201f2
NicDun <nicolas.dunke@gmail.com>
2024-04-07 15:43:16 +0200

    revert so SSG.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import alpinejs from "@astrojs/alpinejs";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 import expressiveCode from 'astro-expressive-code';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
@@ -41,10 +40,4 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime]
   },
   prefetch: true,
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
 });
